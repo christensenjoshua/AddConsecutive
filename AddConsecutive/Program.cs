1eb07e6a41b18ed0bb2f43cd5ee0318d0eba2739
christensenjoshua <christensen.joshua@outlook.com>
2018-10-17 18:12:53 -0600

    Working

--- AddConsecutive/Program.cs
+++ AddConsecutive/Program.cs
@@ -10,6 +10,34 @@ namespace AddConsecutive
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(AddConsecutive(new int[] { 1, 1, 1, 1 }));
+            Console.ReadLine();
+        }
+        static int AddConsecutive(int[] nums)
+        {
+            int firstNum = 0;
+            int lastNum = 0;
+            int output = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int thisNum = nums[i];
+                if(i == 0)
+                {
+                    firstNum = thisNum;
+                    lastNum = thisNum;
+                    continue;
+                }
+                if(thisNum == lastNum)
+                {
+                    output += thisNum;
+                }
+                if (i == nums.Length - 1 && thisNum == firstNum)
+                {
+                    output += thisNum;
+                }
+                lastNum = thisNum;
+            }
+            return output;
         }
     }
 }
